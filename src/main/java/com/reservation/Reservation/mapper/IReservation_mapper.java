989a4df73e178ddbb76a1c14217608a87079fe43
WAJIII\wajahat <wajahatalifarooq.cs@gmail.com>
2022-06-11 20:40:02 +0200

    Changes in Mapper

---
+++ src/main/java/com/reservation/Reservation/mapper/IReservation_mapper.java
@@ -0,0 +1,23 @@
+package com.reservation.Reservation.mapper;
+
+import com.reservation.Reservation.entity.Reservation;
+import com.reservation.Reservation.DTO.ReservationDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import  java.util.List;
+
+@Mapper
+
+public interface IReservation_mapper {
+
+IReservation_mapper INSTANCE = Mappers.getMapper(IReservation_mapper.class);
+
+ReservationDTO mapDTO(Reservation reservation);
+
+Reservation map(ReservationDTO reservationDTO);
+
+List<ReservationDTO> mapList(List<Reservation> reservationsList);
+
+
+}

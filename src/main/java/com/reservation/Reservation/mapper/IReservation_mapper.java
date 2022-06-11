57747da876385613724d509e5a39907fd1b6ce57
WAJIII\wajahat <wajahatalifarooq.cs@gmail.com>
2022-06-11 20:40:02 +0200

    Mapper Dependency Added

---
+++ src/main/java/com/reservation/Reservation/mapper/IReservation_mapper.java
@@ -0,0 +1,15 @@
+package com.reservation.Reservation.mapper;
+
+import com.reservation.Reservation.entity.Reservation;
+import com.reservation.Reservation.DTO.ReservationDTO;
+import org.mapstruct.Mapper;
+
+import  java.util.List;
+
+@Mapper
+
+public interface IReservation_mapper {
+
+
+
+}
